cb9aa5c1d931f7f8e0fae6740b23c70b4756b25b
Song0129 <Song_3191@163.com>
2022-10-25 14:49:51 +0800

    feat: 修复 TS 报错问题

--- 02-merge-sorted-arr/index.ts
+++ 02-merge-sorted-arr/index.ts
@@ -2,7 +2,7 @@ export function merge(A: number[], m: number, B: number[], n: number) {
 	if (!B.length || !n) return;
 
 	for (let i = m; i < m + n; i++) {
-		A[i] = B.shift();
+		A[i] = B.shift()!;
 	}
 
 	// 升序排列
