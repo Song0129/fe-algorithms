270b0cf7d7857f044483bf63b74cbe91548ad6bc
Song0129 <Song_3191@163.com>
2022-10-26 15:12:17 +0800

    feat: 完善合并两个有序数组

--- 02-merge-sorted-arr/index.spec.ts
+++ 02-merge-sorted-arr/index.spec.ts
@@ -1,9 +1,17 @@
 import { test, expect } from "vitest";
-import { merge } from "./index";
+import { mergeOne, mergeTwo } from "./index";
 
-test("merge", () => {
+test("mergeOne", () => {
+	const A: number[] = [1, 2, 3];
+	const B: number[] = [6, 5, 4];
+	mergeOne(A, 3, B, 3);
+	console.log(A);
+	expect(A).toStrictEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test("mergeTwo", () => {
 	const A: number[] = [1, 2, 3];
 	const B: number[] = [6, 5, 4];
 	console.log(A);
-	expect(A).toStrictEqual([1, 2, 3, 4, 5, 6]);
+	expect(mergeTwo(A, 3, B, 3)).toStrictEqual([1, 2, 3, 4, 5, 6]);
 });
--- 02-merge-sorted-arr/index.ts
+++ 02-merge-sorted-arr/index.ts
@@ -1,10 +1,19 @@
-export function merge(A: number[], m: number, B: number[], n: number) {
+export function mergeOne(A: number[], m: number, B: number[], n: number) {
 	if (!B.length || !n) return;
 
 	for (let i = m; i < m + n; i++) {
+		// B.shift()后加 ! 排除B数组为空时 取值为 undefined 类型报错
 		A[i] = B.shift()!;
 	}
 
 	// 升序排列
 	A.sort((a, b) => a - b);
+}
+
+export function mergeTwo(a: number[], m: number, b: number[], n: number) {
+	a = a.splice(0, m > a.length ? a.length : m).concat(new Array(n).fill(0));
+	for (var i = m; i < m + n; i++) {
+		a[i] = b[i - m];
+	}
+	return a.sort();
 }
