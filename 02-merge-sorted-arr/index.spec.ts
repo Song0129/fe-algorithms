08df918b040ee3d37b441edcc7e1111ac9bbecca
Song0129 <Song_3191@163.com>
2022-10-28 11:40:47 +0800

    feat: 新增解法-合并两个有序数组

--- 02-merge-sorted-arr/index.spec.ts
+++ 02-merge-sorted-arr/index.spec.ts
@@ -1,17 +1,33 @@
 import { test, expect } from "vitest";
-import { mergeOne, mergeTwo } from "./index";
+import { mergeOne, mergeTwo, mergeThree } from "./index";
 
-test("mergeOne", () => {
+test("merge1", () => {
 	const A: number[] = [1, 2, 3];
-	const B: number[] = [6, 5, 4];
+	const B: number[] = [2, 5, 6];
 	mergeOne(A, 3, B, 3);
 	console.log(A);
-	expect(A).toStrictEqual([1, 2, 3, 4, 5, 6]);
+	expect(A).toStrictEqual([1, 2, 2, 3, 5, 6]);
 });
 
-test("mergeTwo", () => {
+test("merge2", () => {
 	const A: number[] = [1, 2, 3];
-	const B: number[] = [6, 5, 4];
+	const B: number[] = [2, 5, 6];
 	console.log(A);
-	expect(mergeTwo(A, 3, B, 3)).toStrictEqual([1, 2, 3, 4, 5, 6]);
+	expect(mergeTwo(A, 3, B, 3)).toStrictEqual([1, 2, 2, 3, 5, 6]);
+});
+
+test("merge3", () => {
+	const A: number[] = [1, 2, 3];
+	const B: number[] = [2, 5, 6];
+	mergeThree(A, 3, B, 3);
+	console.log(A);
+	expect(A).toStrictEqual([1, 2, 2, 3, 5, 6]);
+});
+
+test("merge4", () => {
+	const A: number[] = [0];
+	const B: number[] = [1];
+	mergeThree(A, 1, B, 1);
+	console.log(A);
+	expect(A).toStrictEqual([0, 1]);
 });
